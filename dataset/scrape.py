335ac414ec53e2f57afa33d274e28e55b5e7f7fd
Rushab11 <humagainrushab@gmail.com>
2023-09-19 18:51:54 +0545

    minor

--- dataset/scrape.py
+++ dataset/scrape.py
@@ -1,4 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+from tenacity import retry, stop_after_attempt
 
 URL = "https://www.webtoons.com/en/slice-of-life/yumi-cell/list?title_no=478"
+
+class Scrape:
+    def __init__(self) -> None:
+        self.session = requests.Session()
+        self.url = URL
+
